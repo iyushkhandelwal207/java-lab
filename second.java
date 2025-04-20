327a972718c295c8379112cfcf95612234e2e0a7
PIYUSH KHANDELWAL <piyushkhandelwal769@gmail.com>
2025-04-20 07:57:51 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,17 @@
+class second{
+    public static void main(String[] args) {
+        System.out.println(".........................................................jai mata di:");
+        //single line comment
+        /*multi 
+        line
+        comment
+        Arithmatic Operators:+,-,*,/,%(binary),++,--(unary);
+         */ 
+        int x,y,z;
+        x=y=z=2;
+        x+=y;
+        y-=z;
+        z/=(x+y);
+
+    }
+}
